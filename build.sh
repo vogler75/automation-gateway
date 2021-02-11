00da9a3dd262cdfc18007e085d78fb7dec4ba53b
Andreas Vogler <andreas.vogler@me.com>
2021-02-11 21:45:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-mvn package org.apache.maven.plugins:maven-assembly-plugin:2.2:single -Dmaven.test.skip=true
+mvn org.jetbrains.kotlin:kotlin-maven-plugin:1.4.21:compile
+mvn org.apache.maven.plugins:maven-assembly-plugin:2.2:single -Dmaven.test.skip=true

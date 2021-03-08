f6b012185f36653c9dd841640423aea84b9df2eb
Andreas Vogler <andreas.vogler@me.com>
2021-03-08 22:00:42 +0100

    added service handler

---
+++ source/core/src/main/kotlin/at/rocworks/gateway/core/service/ServiceHandler.kt
@@ -0,0 +1,60 @@
+package at.rocworks.gateway.core.service
+
+import io.vertx.core.Future
+import io.vertx.core.Promise
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.servicediscovery.Record
+import io.vertx.servicediscovery.ServiceDiscovery
+
+class ServiceHandler(val vertx: Vertx) {
+    private val discovery = ServiceDiscovery.create(vertx)!!
+    private val observers : MutableMap<String, (Record)->Unit> = mutableMapOf()
+
+    init {
+        onServiceChanged {
+            observers[idOf(it)]?.invoke(it)
+        }
+    }
+
+    fun observeService(type: String, name: String, result: (record: Record)->Unit) {
+        observers[idOf(type, name)] = result
+        getServiceRecord(type, name).onComplete {
+            if (it.result() != null) result(it.result())
+        }
+    }
+
+    fun registerService(type: String, name: String, endpoint: String): Future<Record> {
+        val promise = Promise.promise<Record>()
+        val record = Record()
+            .setType(type)
+            .setName(name)
+            .setLocation(JsonObject().put("endpoint", endpoint))
+        discovery.publish(record, promise)
+        return promise.future()
+    }
+
+    companion object {
+        fun idOf(record: Record) = "${record.type}/${record.name}"
+        fun idOf(type: String, name: String) = "$type/$name"
+        fun endpointOf(record: Record) = record.location.getString("endpoint", "")!!
+    }
+
+    private fun onServiceChanged(result: (record: Record)->Unit) {
+        vertx.eventBus().consumer<JsonObject>(discovery.options().announceAddress) { message ->
+            result(Record(message.body()))
+        }
+    }
+
+    private fun getServiceRecord(type: String, name: String): Future<Record> {
+        val promise = Promise.promise<Record>()
+        discovery.getRecord({ r -> r.name == name && r.type == type }) { record ->
+            if (record.succeeded() && record.result() != null) {
+                promise.complete(record.result())
+            } else {
+                promise.complete(null)
+            }
+        }
+        return promise.future()
+    }
+}

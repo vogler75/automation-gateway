2f9c4248e381b6f4c162d47815bd7d14da673a77
Andreas Vogler <andreas.vogler@me.com>
2021-07-31 10:48:52 +0200

    Update build.sh for docker hub

--- docker/build.sh
+++ docker/build.sh
@@ -2,31 +2,50 @@
 
 build() {
   app="${1:-app}"
-  dir=${2:-.}
-  app_with_path=../source/$dir/$app/build/distributions/$app.tar 
+  sub=${2:-.}
+  ver=${3:-latest}
+
+  echo $app $sub $ver
+
+  if [ $sub = "cluster" ]; then
+    name="rocworks/automation-gateway:$ver-$app-cluster"
+    app_with_path=../source/$sub/$app/build/distributions/$app.tar 
+  else
+    if [ $sub = "." ]; then
+      name="rocworks/automation-gateway:$ver"
+      app_with_path=../source/$app/build/distributions/$app.tar 
+    else
+      app=$app-$sub	    
+      name="rocworks/automation-gateway:$ver-$sub"
+      app_with_path=../source/$app/build/distributions/$app.tar 
+    fi	   
+  fi    
+
   if [ -f $app_with_path ]; then
     echo $app
     cp $app_with_path ./app.tar
-    docker build --build-arg APP_NAME=$app -t frankenstein-$app .
+    docker build --build-arg APP_NAME=$app -t $name .
     rm ./app.tar
   else
-    echo "Please build the app with gradle first!" 
+    echo "Please build the app ${app_with_path} with gradle first!" 
   fi
 }
 
 app="${1:-none}"
+ver=${2:-latest}
 
-if [ $app = "app" ]; then
-  build app
+if [ $app = "single" ]; then
+  build app . $ver
+  build app plc4x $ver
 elif [ $app = "cluster" ]; then
-  build gateway cluster
-  build opcua cluster
-  build plc4x cluster
-  build dds cluster
-  build cache cluster
-  build influxdb cluster
-  build iotdb cluster
-  build kafka cluster
+  build gateway cluster $ver
+  build opcua cluster $ver
+  build plc4x cluster $ver
+  build dds cluster $ver
+  build cache cluster $ver
+  build influxdb cluster $ver
+  build iotdb cluster $ver
+  build kafka cluster $ver
 else
-  echo "Usage $0 [app|cluster]"
+  echo "Usage $0 [single|cluster] [version]"
 fi

490024833edd140d4b74c36df1635ce45e3121bb
Andreas Vogler <andreas.vogler@me.com>
2022-03-24 08:03:02 +0100

    Updated docker commands for version 1.20

--- docker/build.sh
+++ docker/build.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 build() {
-  app="${1:-app}"
+  app=${1:-app}
   sub=${2:-.}
   ver=${3:-latest}
 
   echo $app $sub $ver
 
-  if [ $sub = "cluster" ]; then
-    name="rocworks/automation-gateway:$ver-$app-cluster"
-    app_with_path=../source/$sub/$app/build/distributions/$app.tar 
+  if [ $sub = "." ]; then
+    name="rocworks/automation-gateway:$ver"
+    app_with_path=../source/$app/build/distributions/$app.tar 
   else
-    if [ $sub = "." ]; then
-      name="rocworks/automation-gateway:$ver"
-      app_with_path=../source/$app/build/distributions/$app.tar 
-    else
-      app=$app-$sub	    
-      name="rocworks/automation-gateway:$ver-$sub"
-      app_with_path=../source/$app/build/distributions/$app.tar 
-    fi	   
-  fi    
+    app=$app-$sub	    
+    name="rocworks/automation-gateway:$ver-$sub"
+    app_with_path=../source/$app/build/distributions/$app.tar 
+  fi	   
 
   if [ -f $app_with_path ]; then
     echo $app
@@ -31,21 +26,7 @@ build() {
   fi
 }
 
-app="${1:-none}"
-ver=${2:-latest}
+ver=${2:-`cat version.txt`}
 
-if [ $app = "single" ]; then
-  build app . $ver
-  build app plc4x $ver
-elif [ $app = "cluster" ]; then
-  build gateway cluster $ver
-  build opcua cluster $ver
-  build plc4x cluster $ver
-  build dds cluster $ver
-  build cache cluster $ver
-  build influxdb cluster $ver
-  build iotdb cluster $ver
-  build kafka cluster $ver
-else
-  echo "Usage $0 [single|cluster] [version]"
-fi
+build app . $ver
+build app plc4x $ver
--- docker/push.sh
+++ docker/push.sh
@@ -1,7 +1,7 @@
 version=`cat version.txt`
 
 docker push rocworks/automation-gateway:${version}
-docker push rocworks/automation-gateway:latest 
+#docker push rocworks/automation-gateway:latest 
 
 docker push rocworks/automation-gateway:${version}-plc4x
-docker push rocworks/automation-gateway:latest-plc4x 
+#docker push rocworks/automation-gateway:latest-plc4x 
